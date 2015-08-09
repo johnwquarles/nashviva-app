4416e4fa8d2803b3bd97f6566aa52b68653618c6
johnwquarles <john.w.quarles@gmail.com>
2015-08-09 14:40:53 -0500

    firebase ref factory, loading factory

--- www/js/factories.js
+++ www/js/factories.js
@@ -1,1 +1,21 @@
 angular.module('nashviva.factories', [])
+
+.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var fb = new Firebase("https://dropoff.firebaseio.com/");
+    return $firebaseAuth(fb);
+  }
+])
+
+.factory("Loading", function($ionicLoading) {
+  return {
+    show : function() {
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
+    },
+    hide : function(){
+      $ionicLoading.hide();
+    }
+  }
+})
