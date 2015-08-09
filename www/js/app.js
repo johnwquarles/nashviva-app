af07fdf586212b7cc39aee6f84cd2c501489f058
Meg Cunningham <meg_cunningham@live.com>
2015-08-09 15:48:27 -0500

    routed tabs with views

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,41 @@ angular.module('nashviva', ['ionic', 'firebase', 'ngCordova', 'nashviva.factorie
     }
   });
 })
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.map', {
+    url: '/map',
+    views: {
+      'tab-map': {
+        templateUrl: 'templates/map.html',
+        controller: 'MapCtrl'
+      }
+    }
+  })
+
+  .state('tab.profile', {
+      url: '/profile',
+      views: {
+        'tab-profile': {
+          templateUrl: 'templates/profile.html',
+          controller: 'ProfileCtrl'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/map')
+
+})
